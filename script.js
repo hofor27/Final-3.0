6e6d6042284d66d91ebc9e877c367ddb87e5a79e
hofor27 <hofor27@gmail.com>
2023-10-24 12:42:45 -0300

    Local Storage functionality added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const todoList = document.querySelector('.todo-list');
   const filterOption = document.querySelector('.filter-todo');
 
+  // Load tasks from local storage when the page loads
+  loadTasksFromLocalStorage();
+
   todoButton.addEventListener('click', addTodo);
   todoList.addEventListener('click', deleteCheck);
   filterOption.addEventListener('change', filterTodo);
@@ -15,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     todoDiv.classList.add('todo');
 
     const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value;
+    newTodo.innerText = todoInput.value; // Corrected to display the text
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
 
@@ -31,6 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     todoList.appendChild(todoDiv);
     todoInput.value = '';
+
+    // Save the task to local storage
+    saveTasksToLocalStorage();
   }
 
   function deleteCheck(event) {
@@ -39,17 +45,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (item.classList.contains('trash-btn')) {
       todo.remove();
+
+      // Remove the task from local storage
+      saveTasksToLocalStorage();
     }
 
     if (item.classList.contains('check-btn')) {
       todo.classList.toggle('completed');
+
+      // Update task completion status in local storage
+      saveTasksToLocalStorage();
     }
   }
 
   function filterTodo() {
     const todos = document.querySelectorAll('.todo');
 
-    todos.forEach(function(todo) {
+    todos.forEach(function (todo) {
       switch (filterOption.value) {
         case 'all':
           todo.style.display = 'flex';
@@ -69,6 +81,50 @@ document.addEventListener('DOMContentLoaded', () => {
           }
           break;
       }
+    });
+  }
+
+  function saveTasksToLocalStorage() {
+    const todos = document.querySelectorAll('.todo');
+    const tasks = [];
+
+    todos.forEach(function (todo) {
+      tasks.push({
+        text: todo.querySelector('.todo-item').innerText,
+        completed: todo.classList.contains('completed')
+      });
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  function loadTasksFromLocalStorage() {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    tasks.forEach(function (task) {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add('todo');
+
+      const newTodo = document.createElement('li');
+      newTodo.innerText = task.text; // Corrected to display the text
+      newTodo.classList.add('todo-item');
+      todoDiv.appendChild(newTodo);
+
+      if (task.completed) {
+        todoDiv.classList.add('completed');
+      }
+
+      const completedButton = document.createElement('button');
+      completedButton.innerHTML = '<i class="fas fa-check"></i>';
+      completedButton.classList.add('check-btn');
+      todoDiv.appendChild(completedButton);
+
+      const trashButton = document.createElement('button');
+      trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+      trashButton.classList.add('trash-btn');
+      todoDiv.appendChild(trashButton);
+
+      todoList.appendChild(todoDiv);
     });
   }
 });
