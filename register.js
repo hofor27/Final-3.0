12c58e206e7bfc0516ed3084a58ecdeeabd8b814
haseebasif <haseebasif215@gmail.com>
2023-11-08 19:54:47 +0500

    Register Page Completed

--- register.js
+++ register.js
@@ -1,24 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const RegisterForm = document.getElementById("registerForm");
+  const registerForm = document.getElementById("registerForm");
 
-  RegisterForm.addEventListener("submit", function (event) {
+  registerForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // For demo purposes, assume authentication is successful
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
 
-    // Validate username and password (add your validation logic here)
-    if (username === "example" && password === "password") {
-      // Store login status in localStorage
-      localStorage.setItem("isLoggedIn", "true");
-      localStorage.setItem("username", username); // Store username in localStorage
-
-      // Redirect to index.html
-      window.location.href = "index.html";
-    } else {
-      alert("Invalid username or password. Please try again.");
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      alert("Passwords do not match. Please try again.");
+      return;
     }
+
+    // Here, you can add additional validation logic for username and password, such as length requirements, special characters, etc.
+    // If the validation fails, display an error message and return.
+
+    // For the demo, assume the registration is successful, and store user data in localStorage.
+    // You can replace this with a server-side registration process in a real application.
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    alert("Registration successful. You can now log in with your credentials.");
+
+    // Redirect to the login page after successful registration
+    window.location.href = "login.html";
   });
 });
